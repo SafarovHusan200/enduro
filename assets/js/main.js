d9ddc47b4131da94581dc8e0c13171d989e735f5
SafarovHusan200 <Safarovhusan2000@gmail.com>
2025-02-16 14:29:09 +0500

    desktop complate

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,35 @@ window.addEventListener("DOMContentLoaded", () => {
   close__modal.addEventListener("click", () => {
     modal__parent.classList.remove("open");
   });
+
+  // Swiper
+  var swiper = new Swiper(".clientSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+
+      console.log(panel.classList[1] === "show");
+      if (panel.classList[1] === "show") {
+        panel.classList.remove("show");
+      } else {
+        panel.classList.add("show");
+      }
+    });
+  }
 });
